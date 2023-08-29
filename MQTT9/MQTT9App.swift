b625eef25fe3bec1f52ca156e8a5f98bc347aa7f
ray060101 <qwerqwer0601@gmail.com>
2023-08-29 10:17:58 +0800

    Initial Commit

---
+++ MQTT9/MQTT9App.swift
@@ -0,0 +1,17 @@
+//
+//  MQTT9App.swift
+//  MQTT9
+//
+//  Created by 林宇瑞 on 2023/8/29.
+//
+
+import SwiftUI
+
+@main
+struct MQTT9App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
